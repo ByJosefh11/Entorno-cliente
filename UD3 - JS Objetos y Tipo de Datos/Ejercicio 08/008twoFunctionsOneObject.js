fe7a7c9b4f84bbe372934ed647eed4bc0540da27
Jose Fernandez <115531745+ByJosefh11@users.noreply.github.com>
2022-11-10 09:17:16 +0100

    Delete UD3 - JS Objetos y Tipo de Datos directory

--- UD3 - JS Objetos y Tipo de Datos/Ejercicio 08/008twoFunctionsOneObject.js
+++
@@ -1,9 +0,0 @@
-//Si se cumple. Creo una nueva variable y en la funcion a y b pido que la devuelvan. 
-
-let c={};
-
-function A() { return c }
-function B() { return c }
-let a = new A();
-let b = new B();
-alert( a == b ); // true
